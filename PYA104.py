1998f3b08adcc135521a0c6bea5f3093ccd7dc8d
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:23:40 +0800

    Add files via upload

---
+++ PYA104.py
@@ -0,0 +1,19 @@
+import math
+from math import pi
+
+# TODO
+
+radius = eval(input())
+
+# TODO
+lenth_circle=2*(math.pi)*radius
+area_circle=(math.pi)*(radius**2)
+
+print(f'Radius = {radius:.2f}')
+print(f'Perimeter = {lenth_circle:.2f}')
+print(f'Area = {area_circle:.2f}')
+"""
+Radius = _
+Perimeter = _
+Area = _
+"""
