dd8c1d47db89cb93b238c72eccbdf35e3bf335a7
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:28:54 +0800

    Update and rename PYA107.py to 數值計算.py

--- 數值計算.py
+++ 數值計算.py
@@ -3,6 +3,12 @@ num2 = eval(input())
 num3 = eval(input())
 num4 = eval(input())
 num5 = eval(input())
+'''
+請撰寫一程式，讓使用者輸入五個數字，計算並輸出這五個數字之數值、總和及平均數。
+
+提示：總和與平均數皆輸出到小數點後第1位。
+'''
+
 
 # TODO
 s=num1+num2+num3+num4+num5
