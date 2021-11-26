60cd219a51d0ee5dcfb1309beb3dd7bd120552f2
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:28:30 +0800

    Add files via upload

---
+++ PYA107.py
@@ -0,0 +1,16 @@
+num1 = eval(input())
+num2 = eval(input())
+num3 = eval(input())
+num4 = eval(input())
+num5 = eval(input())
+
+# TODO
+s=num1+num2+num3+num4+num5
+a=s/5
+print(f'{num1} {num2} {num3} {num4} {num5}')
+print(f'Sum = {s:.1f}')
+print(f'Average = {a:.1f}')
+"""
+Sum = _
+Average = _
+"""
