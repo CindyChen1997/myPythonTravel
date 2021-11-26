c8f96353b85d72a4b5a71c7eef0da0d5bb355814
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:21:52 +0800

    Add files via upload

---
+++ PYA103.py
@@ -0,0 +1,14 @@
+# TODO
+ph1=input()
+ph2=input()
+ph3=input()
+ph4=input()
+
+# 靠右對齊
+# TODO
+print('|{0:>10s} {1:>10s}|'.format(ph1,ph2))
+print('|{0:>10s} {1:>10s}|'.format(ph3,ph4))
+# 靠左對齊
+print('|{0:10s} {1:10s}|'.format(ph1,ph2))
+print('|{0:10s} {1:10s}|'.format(ph3,ph4))
+# TODO
