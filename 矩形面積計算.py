a4870905525717768e4c6c75a87e5db5dea856e6
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:25:59 +0800

    Update and rename PYA105.py to 矩形面積計算.py

---
+++ 矩形面積計算.py
@@ -0,0 +1,23 @@
+h = eval(input())
+w = eval(input())
+# TODO
+'''
+請撰寫一程式，輸入兩個正數，代表一矩形之寬和高，計算並輸出此矩形之高（Height）、寬（Width）、周長（Perimeter）及面積（Area）。
+
+提示：輸出浮點數到小數點後第二位。
+'''
+
+print(f'Height = {h:.2f}')
+print(f'Width = {w:.2f}')
+p=(h+w)*2
+print(f'Perimeter = {p:.2f}')
+a=h*w
+print(f'Area = {a:.2f}')
+
+
+"""
+Height = _
+Width = _
+Perimeter = _
+Area = _
+"""
