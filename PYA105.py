b491b3f1fef6f0325deffe22de2ef118696eed22
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:25:22 +0800

    Add files via upload

---
+++ PYA105.py
@@ -0,0 +1,18 @@
+h = eval(input())
+w = eval(input())
+# TODO
+
+print(f'Height = {h:.2f}')
+print(f'Width = {w:.2f}')
+p=(h+w)*2
+print(f'Perimeter = {p:.2f}')
+a=h*w
+print(f'Area = {a:.2f}')
+
+
+"""
+Height = _
+Width = _
+Perimeter = _
+Area = _
+"""
