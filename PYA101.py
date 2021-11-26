aab495f0b26a4b42599f9b6f277a65cc82d5c46e
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:09:54 +0800

    Add files via upload

---
+++ PYA101.py
@@ -0,0 +1,15 @@
+#TODO
+num1=int(input())
+num2=int(input())
+num3=int(input())
+num4=int(input())
+
+#向右靠齊
+#TODO
+print('|{0:5d} {1:5d}|'.format(num1,num2))
+print('|{0:5d} {1:5d}|'.format(num3,num4))
+
+#向左靠齊
+print('|{0:<5d} {1:<5d}|'.format(num1,num2))
+print('|{0:<5d} {1:<5d}|'.format(num3,num4))
+#TODO
