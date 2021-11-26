e68672f6c0c1c3e2133fb7c9ab7897228b0502bb
CindyChen1997 <94896709+CindyChen1997@users.noreply.github.com>
2021-11-26 20:33:13 +0800

    Add files via upload

---
+++ PYA110.py
@@ -0,0 +1,17 @@
+import math
+from math import pow
+from math import tan
+
+# TODO
+
+n = eval(input())
+s = eval(input())
+
+# TODO
+
+a = (n*(s**2))/(4*tan(math.pi/n))
+
+print(f'Area = {a:.4f}')
+"""
+Area = _
+"""
